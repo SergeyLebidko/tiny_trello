ce3c434bf47c705b05171245e3621fea66520990
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-23 16:59:34 +0300

    Добавлен первый маршрут. Удалена настройка homepage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import Main from '../pages/Main/Main';
 import './App.scss';
 
 function App() {
     return (
         <div>
             <h1>Tiny Trello</h1>
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+            </Routes>
         </div>
     );
 }
---
+++ src/components/pages/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.scss';
+
+const Main: React.FC = () => {
+    return (
+        <div>
+            Это главная страница
+        </div>
+    );
+}
+
+export default Main;
