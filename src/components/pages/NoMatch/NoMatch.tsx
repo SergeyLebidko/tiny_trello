0e75c706c73418a6653a4816d440b4db1c2a0eab
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-23 17:24:33 +0300

    В NoMatch.tsx добавлено отображение пути

--- src/components/pages/NoMatch/NoMatch.tsx
+++ src/components/pages/NoMatch/NoMatch.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {useLocation} from 'react-router-dom';
 import './NoMatch.scss';
 
 const NoMatch: React.FC = () => {
+    const location = useLocation();
+
     return (
         <div>
-            Страница не найдена...
+            Страница {location.pathname} не найдена...
         </div>
     );
 }
