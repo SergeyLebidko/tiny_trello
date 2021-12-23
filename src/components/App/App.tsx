41274e2f9d81b0b46a000c706969e6e0690011d8
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-23 18:44:27 +0300

    В App добавлено подключение к store

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import {Route, Routes} from 'react-router-dom';
 import Main from '../pages/Main/Main';
 import BoardList from '../pages/BoardList/BoardList';
 import Board from '../pages/Board/Board';
 import NoMatch from '../pages/NoMatch/NoMatch';
+import store from '../../store/store';
 import './App.scss';
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Main/>}/>
-            <Route path="/board_list" element={<BoardList/>}/>
-            <Route path="/board/:id" element={<Board/>}/>
-            <Route path="*" element={<NoMatch/>}/>
-        </Routes>
+        <Provider store={store}>
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/board_list" element={<BoardList/>}/>
+                <Route path="/board/:id" element={<Board/>}/>
+                <Route path="*" element={<NoMatch/>}/>
+            </Routes>
+        </Provider>
     );
 }
 
