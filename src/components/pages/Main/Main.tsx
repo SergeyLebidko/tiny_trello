3f62ce3e0ad5b74d3553349669249674eea62136
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-23 19:13:43 +0300

    Модалки подлючены в Main

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Register from '../../modals/Register/Register';
+import Login from '../../modals/Login/Login';
+import Logout from '../../modals/Logout/Logout';
 import './Main.scss';
 
+enum ModalMode {
+    NoModal,
+    RegisterModal,
+    LoginModal,
+    LogoutModal
+}
+
 const Main: React.FC = () => {
+    const [modalMode, setModalMode] = useState<ModalMode>(ModalMode.NoModal);
+
+    const showRegister = (): void => setModalMode(ModalMode.RegisterModal);
+    const showLogin = (): void => setModalMode(ModalMode.LoginModal);
+    const showLogout = (): void => setModalMode(ModalMode.LogoutModal);
+
     return (
-        <div>
-            <h1>Tiny Trello</h1>
-            <h3>Это главная страница</h3>
+        <div className="main">
+            {modalMode === ModalMode.RegisterModal && <Register/>}
+            {modalMode === ModalMode.LoginModal && <Login/>}
+            {modalMode === ModalMode.LogoutModal && <Logout/>}
+            <h1>Tiny Trello (Главная страница)</h1>
+            <button onClick={showRegister}>Регистрация</button>
+            <button onClick={showLogin}>Вход</button>
+            <button onClick={showLogout}>Выход</button>
         </div>
     );
 }
