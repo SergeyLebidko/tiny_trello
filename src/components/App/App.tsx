4be477c741937c5afcb8803a2e488acb9bc793d1
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-23 21:36:50 +0300

    Добавлена фильтрация маршрутов в зависимости от пользователя

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {Route, Routes} from 'react-router-dom';
 import Main from '../pages/Main/Main';
 import BoardList from '../pages/BoardList/BoardList';
 import Board from '../pages/Board/Board';
 import NoMatch from '../pages/NoMatch/NoMatch';
-import store from '../../store/store';
+import {User} from '../../store/user/types';
+import {RootState} from '../../store/store';
+import {getUser} from '../../store/selectors';
 import './App.scss';
 
 function App() {
+    const user = useSelector<RootState, User | null>(getUser);
+
     return (
-        <Provider store={store}>
-            <Routes>
-                <Route path="/" element={<Main/>}/>
+        <Routes>
+            <Route path="/" element={<Main/>}/>
+            {/* Пути к списку досок и отдельным доскам доступны только для залогинившихся пользователей */}
+            {user &&
+            <>
                 <Route path="/board_list" element={<BoardList/>}/>
                 <Route path="/board/:id" element={<Board/>}/>
-                <Route path="*" element={<NoMatch/>}/>
-            </Routes>
-        </Provider>
+            </>
+            }
+            <Route path="*" element={<NoMatch/>}/>
+        </Routes>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './components/App/App';
+import store from './store/store';
+import {Provider} from 'react-redux';
 import './style/reset.scss';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
