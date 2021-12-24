ba25f0586bbdffac45abc3476590f17eb6afdc81
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-24 11:27:45 +0300

    В форму регистрации добавлены основные поля

--- src/components/modals/Register/Register.tsx
+++ src/components/modals/Register/Register.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import {createRandomString} from '../../../utils/common';
 import './Register.scss';
 
 const Register: React.FC = () => {
+    const loginId = createRandomString();
+    const firstNameId = createRandomString();
+    const lastNameId = createRandomString();
+    const password1Id = createRandomString();
+    const password2Id = createRandomString();
+
     return (
         <div className="register">
             <div className="register__content">
-                Форма регистрации пользователя
+                <ul>
+                    <li>
+                        <label htmlFor={loginId}>Логин:</label>
+                        <input id={loginId}/>
+                    </li>
+                    <li>
+                        <label htmlFor={firstNameId}>Имя:</label>
+                        <input id={firstNameId}/>
+                    </li>
+                    <li>
+                        <label htmlFor={lastNameId}>Фамилия:</label>
+                        <input id={lastNameId}/>
+                    </li>
+                    <li>
+                        <label htmlFor={password1Id}>Пароль:</label>
+                        <input id={password1Id} type="password"/>
+                    </li>
+                    <li>
+                        <label htmlFor={password2Id}>Пароль:</label>
+                        <input id={password2Id} type="password"/>
+                    </li>
+                </ul>
+                <div>
+                    <button>Отмена</button>
+                    <button>Зарегистрироваться</button>
+                </div>
             </div>
         </div>
     );
