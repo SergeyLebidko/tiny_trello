d27f9764cc8229e85296308b2cd1e70385a97201
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-24 13:10:25 +0300

    Добавлена логика для модалки Login.tsx

--- src/components/modals/Login/Login.tsx
+++ src/components/modals/Login/Login.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {createRandomString} from '../../../utils/common';
+import {useModalError} from '../../../utils/hooks';
 import './Login.scss';
 
 type LoginProps = {
@@ -6,21 +8,53 @@ type LoginProps = {
 }
 
 const Login: React.FC<LoginProps> = ({closeHandler}) => {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    const [error, setErrorText] = useModalError();
+
+    const loginRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const loginId = createRandomString();
+    const passwordId = createRandomString();
+
+    const showPasswordHandler = (): void => setShowPassword(oldValue => !oldValue);
+
+    const loginHandler = (): void => {
+        if (!loginRef.current || !passwordRef.current) return;
+
+        const login = loginRef.current.value;
+        const password = passwordRef.current.value;
+
+        if (!login || !password) {
+            setErrorText('Все поля обязательны к заполнению');
+            return;
+        }
+
+        // TODO Вставить код запроса к "серверу" и добавления пользователя в хранилище redux
+    }
+
     return (
         <div className="login">
             <div className="login__content">
                 <h1>Введите учетные данные</h1>
+                {error && <p>{error}</p>}
                 <ul>
                     <li>
-                        
+                        <label htmlFor={loginId}>Логин:</label>
+                        <input id={loginId} ref={loginRef}/>
                     </li>
                     <li>
-
+                        <button onClick={showPasswordHandler}>
+                            {showPassword ? 'Скрыть пароль' : 'Показать пароль'}
+                        </button>
+                        <label htmlFor={passwordId}>Пароль:</label>
+                        <input id={passwordId} ref={passwordRef} type={showPassword ? 'text' : 'password'}/>
                     </li>
                 </ul>
                 <div>
                     <button onClick={closeHandler}>Отмена</button>
-                    <button>Войти</button>
+                    <button onClick={loginHandler}>Войти</button>
                 </div>
             </div>
         </div>
