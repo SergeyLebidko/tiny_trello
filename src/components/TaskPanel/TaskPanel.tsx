3810898bca54913ea76c1187c30e1673eb7c64dc
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-27 12:13:51 +0300

    Скорректирован вывод даты в карточках

--- src/components/TaskPanel/TaskPanel.tsx
+++ src/components/TaskPanel/TaskPanel.tsx
@@ -10,12 +10,20 @@ const TaskPanel: React.FC<Task> = ({text, done, importance, deadline}) => {
         [Importance.High]: 'Высокая'
     }
 
+    const getFormattedDate = (timestamp: number): string => {
+        const date = new Date(timestamp);
+        const d = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+        const m = date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+        const y = date.getFullYear();
+        return `${d}.${m}.${y}`
+    }
+
     return (
         <li className="task_panel">
             <h1>{text}</h1>
             <h2>{done ? 'Выполнено' : 'Не выполнено'}</h2>
             <h2>Важность: {IMPORTANCE_TEXT_SELECTOR[importance]}</h2>
-            <h2>Срок: {deadline}</h2>
+            <h2>Срок: {getFormattedDate(deadline)}</h2>
         </li>
     );
 };
