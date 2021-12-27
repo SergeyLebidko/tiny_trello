c91081301d8a1becbc410c7624d1c26cb070dc90
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-27 14:37:47 +0300

    Обновлены экшены

--- src/store/card/actions.ts
+++ src/store/card/actions.ts
@@ -1,12 +1,36 @@
 import {Dispatch} from 'redux';
 import backend from '../../backend/backend';
-import {CardAction, CardActionTypes} from './types';
+import {Card, CardAction, CardActionTypes} from './types';
 
 export const loadCards = () => (dispatch: Dispatch<CardAction>): void => {
     const cards = backend.getCards();
     dispatch({
         type: CardActionTypes.SetCardList,
         payload: cards
+    });
+}
+
+export const createCard = (card: Card) => (dispatch: Dispatch<CardAction>): void => {
+    const createdCard = backend.createCard(card);
+    dispatch({
+        type: CardActionTypes.CreateCard,
+        payload: createdCard
+    });
+}
+
+export const patchCard = (card: Card) => (dispatch: Dispatch<CardAction>): void => {
+    const patchedCard = backend.patchCard(card);
+    dispatch({
+        type: CardActionTypes.PatchCard,
+        payload: patchedCard
+    })
+}
+
+export const removeCard = (card: Card) => (dispatch: Dispatch<CardAction>): void => {
+    const removedCard = backend.removeCard(card);
+    dispatch({
+        type: CardActionTypes.RemoveCard,
+        payload: removedCard
     });
 }
 
--- src/store/task/actions.ts
+++ src/store/task/actions.ts
@@ -1,12 +1,36 @@
 import {Dispatch} from 'redux';
 import backend from '../../backend/backend';
-import {TaskAction, TaskActionTypes} from './types';
+import {Task, TaskAction, TaskActionTypes} from './types';
 
 export const loadTasks = () => (dispatch: Dispatch<TaskAction>): void => {
     const tasks = backend.getTasks();
     dispatch({
         type: TaskActionTypes.SetTaskList,
         payload: tasks
+    });
+}
+
+export const createTask = (task: Task) => (dispatch: Dispatch<TaskAction>): void => {
+    const createdTask = backend.createTask(task);
+    dispatch({
+        type: TaskActionTypes.CreateTask,
+        payload: createdTask
+    });
+}
+
+export const patchTask = (task: Task) => (dispatch: Dispatch<TaskAction>): void => {
+    const patchedTask = backend.patchTask(task);
+    dispatch({
+        type: TaskActionTypes.PatchTask,
+        payload: patchedTask
+    })
+}
+
+export const removeTask = (task: Task) => (dispatch: Dispatch<TaskAction>): void => {
+    const removedTask = backend.removeTask(task);
+    dispatch({
+        type: TaskActionTypes.RemoveTask,
+        payload: removedTask
     });
 }
 
