001c295707cb4ddb2542ed88b1b4b370c1d2f2e8
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-27 14:28:15 +0300

    Обновлены редьюсеры

--- src/store/card/reducer.ts
+++ src/store/card/reducer.ts
@@ -1,11 +1,19 @@
 import {Card, CardAction, CardActionTypes} from './types';
 
-export function cardsReducer(state: Array<Card> = [], action: CardAction){
+export function cardsReducer(state: Array<Card> = [], action: CardAction) {
     switch (action.type) {
         case CardActionTypes.SetCardList: {
             return action.payload;
         }
-        //TODO Дополнить редьюсер кодом для выполнения других операций со списоком карточек
+        case CardActionTypes.CreateCard: {
+            return [...state, action.payload];
+        }
+        case CardActionTypes.PatchCard: {
+            return state.map(card => card.id === action.payload.id ? action.payload : card);
+        }
+        case CardActionTypes.RemoveCard: {
+            return state.filter(card => card.id !== action.payload.id);
+        }
         default: {
             return state;
         }
--- src/store/task/reducer.ts
+++ src/store/task/reducer.ts
@@ -1,11 +1,19 @@
 import {Task, TaskAction, TaskActionTypes} from './types';
 
-export function tasksReducer(state: Array<Task> = [], action: TaskAction){
+export function tasksReducer(state: Array<Task> = [], action: TaskAction) {
     switch (action.type) {
         case TaskActionTypes.SetTaskList: {
             return action.payload;
         }
-        //TODO Дополнить редьюсер кодом для выполнения других операций со списоком задач
+        case TaskActionTypes.CreateTask: {
+            return [...state, action.payload];
+        }
+        case TaskActionTypes.PatchTask: {
+            return state.map(task => task.id === action.payload.id ? action.payload : task);
+        }
+        case TaskActionTypes.RemoveTask: {
+            return state.filter(task => task.id !== action.payload.id);
+        }
         default: {
             return state;
         }
