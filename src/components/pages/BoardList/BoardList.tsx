77121d47da5eb3dc449f31a38bd07fb0fb2c18ab
DmitryReztsov <rez_dima@mail.ru>
2021-12-27 18:34:57 +0300

    Реализовано создание шаблона доски

--- src/components/pages/BoardList/BoardList.tsx
+++ src/components/pages/BoardList/BoardList.tsx
@@ -3,9 +3,10 @@ import {Link} from 'react-router-dom';
 import {getBoards, getLoggedUser, useTypedSelector} from '../../../store/selectors';
 import {ROUTE_PREFIX} from '../../../constants/settings';
 import {useDispatch} from 'react-redux';
-import {removeBoard} from '../../../store/board/actions';
+import {createBoard, removeBoard} from '../../../store/board/actions';
 import {Board} from '../../../store/board/types';
 import './BoardList.scss';
+import {User} from "../../../store/user/types";
 
 const BoardList: React.FC = () => {
     const dispatch = useDispatch();
@@ -16,25 +17,40 @@ const BoardList: React.FC = () => {
     const removeBoardHandler = (board: Board): void => {
         dispatch(removeBoard(board));
     }
+    const addBoardHandler = (loggedUser: User) : void => {
+        if (loggedUser.id) {
+            dispatch(createBoard({
+                userId: loggedUser.id,
+                title: 'Какая-то борда'
+            }));
+        }
+    }
 
     return (
-        <div>
-            {loggedUser &&
-            <h1>
-                Здесь будет список досок пользователя {loggedUser.firstName} {loggedUser.lastName}
-            </h1>
-            }
-            <ul>
-                {boards.map(
-                    board =>
-                        <li key={board.title}>
-                            <Link to={`/${ROUTE_PREFIX}/board/${board.id}`}>{board.title}</Link>
-                            <button onClick={() => removeBoardHandler(board)}>Удалить доску</button>
-                        </li>
-                )}
-            </ul>
-        </div>
-    );
+        loggedUser ? (
+            <div>
+                <h1>
+                    Здесь будет список досок пользователя {loggedUser.firstName} {loggedUser.lastName}
+                </h1>
+                <ul style={{display:'inline-flex'}}>
+                    {boards.map(
+                        board =>
+                            <li key={board.id} style={{width:200, height:100, border: '1px solid black'}}>
+                                <Link to={`/${ROUTE_PREFIX}/board/${board.id}`}>{board.title}</Link>
+                                <button onClick={() => removeBoardHandler(board)}>Удалить доску</button>
+                            </li>
+                    )}
+                    <button
+                        style={{width:200, height:100, border: '1px solid black'}}
+                        onClick={() => addBoardHandler(loggedUser)}
+                    >
+                        Создать доску
+                    </button>
+                </ul>
+            </div>
+            )
+            : null
+    )
 }
 
 export default BoardList;
