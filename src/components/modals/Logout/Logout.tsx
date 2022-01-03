16662ee276565f05ddefbaecc43bc56fae70a41b
SergeyLebidko <it.sergeyler@mail.ru>
2022-01-03 18:22:01 +0300

    Удален ошибочний импортв в Logout.tsx

--- src/components/modals/Logout/Logout.tsx
+++ src/components/modals/Logout/Logout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useDispatch} from 'react-redux';
 import {logoutUserAction} from '../../../store/user/actions';
-import './Logout.scss';
 
 type LogoutProps = {
     closeHandler: () => void
