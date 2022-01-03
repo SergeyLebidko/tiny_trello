f53ac34cf7b83c4b16e696d282606578392c1237
DmitryReztsov <rez_dima@mail.ru>
2022-01-03 17:40:14 +0300

    Добавлены стили Confirm.scss

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './components/App/App';
 import store from './store/store';
 import {Provider} from 'react-redux';
 import './backend/backend';
-import './style/reset.scss';
+import './sass/_reset.scss';
 
 ReactDOM.render(
     <Provider store={store}>
