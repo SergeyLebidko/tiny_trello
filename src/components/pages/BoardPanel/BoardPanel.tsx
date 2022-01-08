9a725e3e4d113a7afc77a7ed6b0b563c382c2cf5
SergeyLebidko <it.sergeyler@mail.ru>
2022-01-08 13:33:30 +0300

    Создан CardCreateForm

--- src/components/pages/BoardPanel/BoardPanel.tsx
+++ src/components/pages/BoardPanel/BoardPanel.tsx
@@ -1,94 +1,78 @@
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
 import {Link, useParams} from 'react-router-dom';
 import {Board} from '../../../store/board/types';
 import CardPanel from '../../CardPanel/CardPanel';
+import CardCreateForm from '../../forms/CardCreateForm/CardCreateForm';
 import NoMatch from '../NoMatch/NoMatch';
 import {getBoards, getCards, useTypedSelector} from '../../../store/selectors';
 import {Card} from '../../../store/card/types';
 import {useDispatch} from 'react-redux';
-import {createCard, removeCard} from '../../../store/card/actions';
-import {Importance, Task} from "../../../store/task/types";
-import {createTask} from "../../../store/task/actions";
-import { useImage } from '../../../utils/hooks';
+import {removeCard} from '../../../store/card/actions';
+import {Task} from '../../../store/task/types';
+import {useImage} from '../../../utils/hooks';
 import './BoardPanel.scss';
 
 const BoardPanel: React.FC = () => {
     const dispatch = useDispatch();
-    const { icons } = useImage();
-
     const boards: Array<Board> = useTypedSelector(getBoards);
     const cards: Array<Card> = useTypedSelector(getCards);
 
+    const [currentCard, setCurrentCard] = useState<Card | null>(null);
+    const [currentTask, setCurrentTask] = useState<Task | null>(null);
 
-    const {boardId} = useParams();
+    const [hasCreateForm, setHasCreateForm] = useState<boolean>(false);
+
+    const openCreateForm = (): void => setHasCreateForm(true);
+    const closeCreateFrom = (): void => setHasCreateForm(false);
+
+    const {icons} = useImage();
 
     const removeCardHandler = (card: Card): void => {
         dispatch(removeCard(card));
     }
 
-    const [currentCard,setCurrentCard] = useState<Card | null>(null)
-    const [currentTask,setCurrentTask] = useState<Task | null>(null)
-    const [edit, setEdit] = useState<boolean>(false)
-    const inputRef = useRef<HTMLInputElement>(null)
-
-    // Ищем доску, соответствующую переданному id. Если доски с таким идентификатором не нашлось - выводим страницу NoMatch
-    const board = boards.find(board => board.id === Number(boardId));
-
-    if (!board) return <NoMatch/>;
-
-    function dragStartHandler(e:React.DragEvent<HTMLLIElement>,card: Card, task: Task) {
+    function dragStartHandler(e: React.DragEvent<HTMLLIElement>, card: Card, task: Task) {
         setCurrentCard(card)
         setCurrentTask(task)
     }
 
-    const addCardHandler = (): void => {
-        if (!inputRef.current) return;
-        dispatch(createCard(
-            {
-                boardId: board.id as number,
-                title: inputRef.current.value,
-                order: getNewOrder() as number,
-            }
-        ))
-        setEdit(!edit)
-    }
-    // В зависимости от длины массива карт получаем новую позицию
-    function getNewOrder() {
-        if (!board) return;
-        return cards.filter(card => card.boardId === board.id).length
-    }
+    const {boardId} = useParams();
+    // Ищем доску, по id из url. Если доски с таким id не нашлось - будет выведена страница NoMatch
+    const board = boards.find(board => board.id === Number(boardId));
+
+    if (!board) return <NoMatch/>;
 
     return (
         <div className="boardPanel">
+
             <div className="boardPanel__back">
-                    <Link to="/board_list">
-                        <img 
-                            className="boardPanel__icon_back"
-                            src={icons.iconBack} 
-                            alt="back" 
-                        />
-                    </Link>
+                <Link to="/board_list">
+                    <img
+                        className="boardPanel__icon_back"
+                        src={icons.iconBack}
+                        alt="back"
+                    />
+                </Link>
             </div>
-            
-            <div className="boardPanel__header">            <div className="boardPanel__header_block">
+
+            <div className="boardPanel__header">
+                <div className="boardPanel__header_block">
                     <div className="boardPanel__header_left">
-                        <div className="boardPanel__header_left_item"></div>
+                        <div className="boardPanel__header_left_item"/>
                     </div>
                     <div className="boardPanel__header_middle">
-                        <div className="boardPanel__header__triangle_left"></div>
-                        <div className="boardPanel__header__triangle_right"></div>
+                        <div className="boardPanel__header__triangle_left"/>
+                        <div className="boardPanel__header__triangle_right"/>
                         <Link className="boardPanel__logo" to="/">Tiny-trello</Link>
-                        <div className="boardPanel__title">                                                   
+                        <div className="boardPanel__title">
                             {board.title}
                         </div>
                     </div>
                     <div className="boardPanel__header_right">
-                        <div className="boardPanel__header_right_item"></div>
-                    </div>                       
+                        <div className="boardPanel__header_right_item"/>
+                    </div>
                 </div>
             </div>
-
-                
 
             <ul className="boardPanel__content">
                 {cards
@@ -104,51 +88,22 @@ const BoardPanel: React.FC = () => {
                             removeCardHandler={removeCardHandler}
                         />)
                 }
-                {/*когда мы создаем новый список, появляется форма*/}
-                {edit ?
-                    <li className="cardPanel">
-                        <p className="cardPanel__name">Введите название списка</p>
-                        <input 
-                            className="cardPanel__inp_name"
-                            ref={inputRef} 
-                            autoFocus
-                        />                                
-                        <button
-                            className="cardPanel__btn_confirm"
-                            onClick={addCardHandler}
-                        >
-                            <img 
-                                className="cardPanel__icon_confirm"
-                                src={icons.iconConfirm} 
-                                alt="confirm" 
-                            />
-                        </button>
-                        <button
-                            className="cardPanel__btn_cancel"
-                            onClick={() => setEdit(!edit)}
-                        >
-                            <img 
-                                className="cardPanel__icon_cancel"
-                                src={icons.iconRemove} 
-                                alt="cancel" 
-                            />
-                        </button>
-                    </li>
-                    :                    
-                    <button
-                        className="boardPanel__btn_addCard"
-                        onClick={() => setEdit(!edit)}
-                    >
-                        <img 
+                {hasCreateForm ?
+                    <CardCreateForm board={board} closeHandler={closeCreateFrom}/>
+                    :
+                    <button className="boardPanel__btn_addCard" onClick={openCreateForm}>
+                        <img
                             className="boardPanel__icon_addCard"
-                            src={icons.iconAddCard} 
-                            alt="addCard" 
+                            src={icons.iconAddCard}
+                            alt="addCard"
                         />
-                    </button>                  
+                    </button>
                 }
             </ul>
 
-            <footer className="boardList__footer">Свои предложения по развитию сайта и переводу присылайте на почту kk309@mail.ru</footer>
+            <footer className="boardList__footer">Свои предложения по развитию сайта и переводу присылайте на почту
+                kk309@mail.ru
+            </footer>
         </div>
     );
 }
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,6 @@
 import {LETTERS, ALL_LETTERS} from '../constants/settings';
+import {Card} from '../store/card/types';
+import {Task} from '../store/task/types';
 
 const randomChar = (sequence: string): string => sequence[Math.floor(Math.random() * sequence.length)];
 
@@ -18,4 +20,13 @@ export function createRandomString(size = 8, hasNoDigits = true): string {
 // Защита типа для корректного чтения сообщений об ошибках в блоках catch
 export function isError(e: unknown): e is Error {
     return e instanceof Error;
+}
+
+// Функция получения следующего значения поля порядка
+export function getNextOrder<T extends Card | Task>(objects: Array<T>): number {
+    let result = 0;
+    if(objects.length > 0){
+        result = Math.max(...objects.map(object => object.order)) + 1;
+    }
+    return result;
 }
